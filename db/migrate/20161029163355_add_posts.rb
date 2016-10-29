21b3e0c9dbd1c122e0cd7e8182ba09b1ec95087d
jay0932 <jay0932business@gmail.com>
2016-10-30 00:37:57 +0800

    add migrate posts table

---
+++ db/migrate/20161029163355_add_posts.rb
@@ -0,0 +1,11 @@
+class AddPosts < ActiveRecord::Migration[5.0]
+  def change
+	create_table :posts do |t|
+		t.string :title
+        	t.text   :content
+        	t.integer :user_id
+
+        	t.timestamps null: false
+	end
+  end
+end
