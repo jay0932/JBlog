649c25e91d70b065faa90fb94488a79ef64b4cfe
jay0932 <jay0932business@gmail.com>
2016-10-30 00:40:04 +0800

    add migrate connections table

---
+++ db/migrate/20161029163851_add_connections.rb
@@ -0,0 +1,10 @@
+class AddConnections < ActiveRecord::Migration[5.0]
+  def change
+	create_table :connections do |t|
+		t.integer :post_id
+                t.integer :category_id
+
+                t.timestamps null: false
+	end
+  end
+end
