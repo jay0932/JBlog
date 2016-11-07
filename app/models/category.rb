289de461e05eaccb56b1d51cd4ef3b54cd94f8a1
jay0932 <jay0932business@gmail.com>
2016-11-08 00:51:30 +0800

    edit post category user N TO N part2

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :connections
-  has_many :posts , through :connections
+  has_many :posts, through: :connections
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   
   has_many :connections
-  has_many :categories , through :connections
+  has_many :categories, through: :connections
 
 end
